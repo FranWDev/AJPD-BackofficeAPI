0e0804a07b7b1494ae68cb2fcc8957d26bf0d6e2
FranWDev <hernandezcrosafrancisco@gmail.com>
2025-10-28 23:54:34 +0000

    Implementado sistema de cache resistente a reinicios

--- src/main/java/org/dubini/backofficeAPI/controller/CacheInvalidatorController.java
+++ src/main/java/org/dubini/backofficeAPI/controller/CacheInvalidatorController.java
@@ -1,11 +1,13 @@
 package org.dubini.backofficeAPI.controller;
 
+import org.dubini.backofficeAPI.dto.response.HttpResponse;
 import org.dubini.backofficeAPI.service.CacheInvalidatorService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequiredArgsConstructor
@@ -15,13 +17,12 @@ public class CacheInvalidatorController {
     private final CacheInvalidatorService cacheInvalidatorService;
 
     @GetMapping("/invalidate/news")
-    public void invalidateNewsCache() {
-        cacheInvalidatorService.invalidateNewsCache().subscribe();
+    public Mono<HttpResponse> invalidateNewsCache() {
+        return cacheInvalidatorService.invalidateNewsCache();
     }
 
     @GetMapping("/invalidate/activities")
-    public void invalidateActivitiesCache() {
-        cacheInvalidatorService.invalidateActivitiesCache().subscribe();
+    public Mono<HttpResponse> invalidateActivitiesCache() {
+        return cacheInvalidatorService.invalidateActivitiesCache();
     }
-
 }
